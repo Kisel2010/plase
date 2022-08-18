a59c54c217cf51b9db95810e663efe3a0494d8d2
Kisel2010 <111506196+Kisel2010@users.noreply.github.com>
2022-08-18 17:40:13 +0500

    Update plase-HTML5-Shipping.UE4.js

--- plase-HTML5-Shipping.UE4.js
+++ plase-HTML5-Shipping.UE4.js
@@ -1155,7 +1155,7 @@ $(document).ready(function() {
 		// ----------------------------------------
 		// MORE JS
 		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Kisel2010/plase/a11817b29e56d7e7f8dba0fd6ca2081f729a56c5/plase-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Kisel2010/plase/e25ef69739030670779f8a45d915df26cbbc2e35/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('plase-HTML5-Shipping.data')).then(function(dataBlob) {
